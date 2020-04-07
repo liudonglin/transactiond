e0e122bcbda8561ac6d446aa130986b367b140cf
liudonglin <xiayangqiushi@hotmail.com>
2020-04-07 11:56:56 +0800

    update transaction model value

--- common/transaction_model.go
+++ common/transaction_model.go
@@ -3,6 +3,6 @@ package common
 type TransactionModel int32
 
 const (
-	TransactionModel_Tcc TransactionModel = 0 //tcc模式
-	TransactionModel_Lcn TransactionModel = 1 //lcn模式
+	TransactionModel_Tcc TransactionModel = 1 //tcc模式
+	TransactionModel_Lcn TransactionModel = 2 //lcn模式
 )
