8ceaad8d3da22c88adef5f25ca0e879d95f4bc78
liudonglin <xiayangqiushi@hotmail.com>
2020-04-14 15:47:58 +0800

    update 实现服务端数据推送

--- common/transaction_type.go
+++
@@ -1,8 +0,0 @@
-package common
-
-type TransactionType int32
-
-const (
-	TransactionType_Root   TransactionType = 1 //tcc模式
-	TransactionType_Branch TransactionType = 2 //lcn模式
-)
--- txmsg/grpc_handler.go
+++ txmsg/grpc_handler.go
@@ -3,42 +3,74 @@ package txmsg
 import (
 	"context"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/peer"
 	"log"
 	"net"
+	"transactiond/common"
 )
 
-var groups = make(map[string][]string)
+var groups = make(map[string][]*common.Transaction)
+var clients = make(map[string]chan *NotifyUnitMessage)
 
 type gRpcServer struct{}
 
 func (s *gRpcServer) CreateGroup(ctx context.Context, in *CreateGroupMessage) (*ResponseMessage, error) {
 	log.Printf("create a new group : [%s]", in.GroupId)
-	groups[in.GroupId] = make([]string, 0)
+	groups[in.GroupId] = make([]*common.Transaction, 0)
 	return &ResponseMessage{Code: "200"}, nil
 }
 
 func (s *gRpcServer) JoinGroup(ctx context.Context, in *JoinGroupMessage) (*ResponseMessage, error) {
 	log.Printf("a unit [%s] join the group [%s]", in.UnitId, in.GroupId)
-	if v, ok := groups[in.GroupId]; ok {
-		v = append(v, in.UnitId)
+
+	p, _ := peer.FromContext(ctx)
+	addr := p.Addr.String()
+
+	if infos, ok := groups[in.GroupId]; ok {
+		info := &common.Transaction{
+			GlobalId: in.GroupId,
+			UnitId:   in.UnitId,
+			Source:   addr,
+		}
+		infos = append(infos, info)
+		groups[in.GroupId] = infos
 	}
 	return &ResponseMessage{Code: "200"}, nil
 }
 
 func (s *gRpcServer) NotifyGroup(ctx context.Context, in *NotifyGroupMessage) (*ResponseMessage, error) {
 	log.Printf("group received notify [%s], stats is [%s] ", in.GroupId, in.State.String())
-	if v, ok := groups[in.GroupId]; ok {
-		print(len(v))
+	if infos, ok := groups[in.GroupId]; ok {
+
+		msg := &NotifyUnitMessage{
+			GroupId: in.GroupId,
+			State:   in.State,
+		}
+
+		for _, info := range infos {
+			client := clients[info.Source]
+			client <- msg
+		}
 	}
+
 	return &ResponseMessage{Code: "200"}, nil
 }
 
 func (s *gRpcServer) NotifyUnit(in *NotifyUnitMessage, out ManageService_NotifyUnitServer) error {
-	log.Printf("group notify [%s]", in.GroupId)
-	if v, ok := groups[in.GroupId]; ok {
-		print(len(v))
+	p, _ := peer.FromContext(out.Context())
+	addr := p.Addr.String()
+
+	log.Printf("a new client connection address is [%s]", addr)
+
+	notifyChan := make(chan *NotifyUnitMessage)
+	clients[addr] = notifyChan
+
+	for {
+		select {
+		case msg := <-notifyChan:
+			out.Send(msg)
+		}
 	}
-	return nil
 }
 
 func StartRpcHandler() {
